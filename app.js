aa6e7eecc3b2c825aa144b68b8e9b78b6ae593db
firdavsxon@gmail.com <firdavsxon@gmail.com>
2024-08-31 02:16:25 +0500

    push

---
+++ app.js
@@ -0,0 +1,20 @@
+// const button = document.getElementById("button");
+// const hi = document.getElementById("hi");
+
+// let isBlack turu;
+
+// button && button.addEventListener("click", function(event)) {
+//     eval.preventDefault ();
+
+//     if (isBlack) {
+//         hi.style.color ="red";
+//         isBlack = false;
+//     }
+//     else {
+//         hi.style.color = "black";
+//         isBlack turu;
+
+//     }
+// }
+
+
